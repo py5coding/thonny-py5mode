020918f2adf418302df6a15f3013ff85be48caf7
GoToLoop <EMLF@HotMail.com>
2025-09-10 17:33:36 -0300

    Constant docstrings + comment & style refactorings

--- thonnycontrib/thonny-py5mode/install_jdk.py
+++ thonnycontrib/thonny-py5mode/install_jdk.py
@@ -1,6 +1,5 @@
-'''thonny-py5mode JDK installer
-   checks for JDK and, if not found, installs it to the Thonny config directory
-'''
+'''thonny-py5mode JDK installer.
+Checks for JDK and, if not found, installs it to Thonny's user directory.'''
 
 import re, shutil, jdk
 
@@ -10,7 +9,7 @@ from threading import Thread
 from os import environ as env, scandir, rename, PathLike
 from os.path import islink, realpath
 
-from typing import Callable, Literal, TypeAlias
+from typing import Any, Callable, Literal, TypeAlias
 from collections.abc import Iterable, Iterator
 
 import tkinter as tk
@@ -23,7 +22,7 @@ from thonny.languages import tr
 StrPath: TypeAlias = str | PathLike[str]
 '''A type representing string-based filesystem paths.'''
 
-PathAction: TypeAlias = Callable[[StrPath], None]
+PathAction: TypeAlias = Callable[[StrPath], Any]
 '''Represents an action applied to a single path-like object.'''
 
 JDK_PATTERN = re.compile(r"""
@@ -31,30 +30,44 @@ JDK_PATTERN = re.compile(r"""
     -?              # Match optional hyphen '-'
     (\d+)           # Capture JDK major version number as group(1)
 """, re.IGNORECASE | re.VERBOSE)
+'''Captures the major version number from strings like "java-17" or "jdk21".'''
 
-REQUIRE_JDK, VERSION_JDK = 17, '17' # Minimum required version
-DOWNLOAD_JDK = '21' # JDK version to download
-JDK_DIR = 'jdk-' + DOWNLOAD_JDK # JDK subfolder name
+REQUIRE_JDK, VERSION_JDK = 17, '17'
+'''JDK minimum required version to run Processing.'''
 
-THONNY_USER_PATH = Path(THONNY_USER_DIR) # Thonny folder's full path string
-JDK_PATH = THONNY_USER_PATH / JDK_DIR # Path for JDK subfolder
-JDK_HOME = str(JDK_PATH) # JDK subfolder's full path string
+DOWNLOAD_JDK = '21'
+'''JDK version to download.'''
 
-WORKBENCH = get_workbench() # Workbench singleton instance
+JDK_DIR = 'jdk-' + DOWNLOAD_JDK
+'''JDK install subfolder name.'''
+
+THONNY_USER_PATH = Path(THONNY_USER_DIR)
+'''Thonny user folder's full path string.'''
+
+JDK_PATH = THONNY_USER_PATH / JDK_DIR
+'''Path for JDK installation subfolder.'''
+
+JDK_HOME = str(JDK_PATH)
+'''JDK install subfolder's full path string.'''
+
+WORKBENCH = get_workbench()
+'''Thonny's workbench singleton instance.'''
 
 def install_jdk() -> None: # Module's main entry-point function
     '''Call this function from where this module is imported.'''
-    if is_java_home_set(): return # JAVA_HOME points to a required JDK version
+
+    if is_java_home_set(): return # JAVA_HOME already points to required version
 
     # Set a local JAVA_HOME to the detected JDK found in THONNY_USER_DIR:
     if path := get_thonny_jdk_install(): set_java_home(path)
 
     # Otherwise, if Thonny doesn't have a proper JDK version...
-    else: ui_utils.show_dialog(JdkDialog()) # ... ask permission to download it.
+    else: ui_utils.show_dialog(JdkDialog()) # ... ask permission to download 1.
 
 
 def is_java_home_set() -> bool:
     '''Check system for existing JDK that meets the py5 version requirements.'''
+
     if java_home := env.get('JAVA_HOME'): # Check if JAVA_HOME is already set
         system_jdk = 'TBD' # JDK version To-Be-Determined
 
@@ -73,6 +86,7 @@ def is_java_home_set() -> bool:
 def get_thonny_jdk_install() -> PurePath | Literal['']:
     '''Check Thonny's user folder for a JDK installation subfolder
     and return its path. Otherwise, return an empty string.'''
+
     for subfolder in get_all_thonny_folders(): # Loop over each subfolder name
         # Use regexp to check if subfolder contains a valid JDK name: 
         if match := JDK_PATTERN.search(subfolder):
@@ -89,8 +103,9 @@ def get_thonny_jdk_install() -> PurePath | Literal['']:
 
 def set_java_home(jdk_path: StrPath) -> None:
     '''Add JDK path to config file (tools > options > general > env vars).'''
+
     jdk_path = str(adjustjdk_path(jdk_path)) # Platform-adjusted path
-    env['JAVA_HOME'] = jdk_path # Python's process points to Thonny's JDK
+    env['JAVA_HOME'] = jdk_path # Python's process points to Thonny's JDK too
 
     jdk_path_entry = create_java_home_entry_from_path(jdk_path)
     env_vars: set[str] = set(WORKBENCH.get_option('general.environment'))
@@ -104,6 +119,7 @@ def set_java_home(jdk_path: StrPath) -> None:
 
 def adjustjdk_path(jdk_path: StrPath) -> PurePath:
     '''Adjust JDK path for the specificity of current platform.'''
+
     jdk_path = PurePath(jdk_path)
 
     # if MacOS, append "/Contents/Home/" to form the actual JDK path for it:
@@ -147,12 +163,11 @@ def get_all_thonny_folders() -> list[str]:
 
 class JdkDialog(ui_utils.CommonDialog):
     '''User-facing dialog prompting install of required JDK for py5 sketches.
-
     - Presents user with option to proceed or cancel the JDK installation.
     - Displays a horizontal indeterminate-sized progress bar during download.
     - Launches a background thread to handle installation tasks.
-    - Shows a success message when installation is complete.
-    '''
+    - Shows a success message when installation is complete.'''
+
     _TITLE = tr('Install JDK ' + DOWNLOAD_JDK + ' for py5')
 
     _PROGRESS = tr('Downloading and extracting JDK ' + DOWNLOAD_JDK + ' ...')
@@ -166,70 +181,65 @@ class JdkDialog(ui_utils.CommonDialog):
         "Thonny requires at least JDK " + VERSION_JDK + " to run py5 sketches. "
         "It'll need to download about 180 MB.")
 
-    _PAD = 0, 15
+    _PROGRESS_BAR_Y_PADDING = 0, 15
 
     def __init__(self, master=WORKBENCH, skip_diag_attribs=False, **kw):
         super().__init__(master, skip_diag_attribs, **kw)
 
-        # Window/Frame:
-        self.main_frame = ttk.Frame(self)
-        self.main_frame.grid(ipadx=15, ipady=15, sticky=tk.NSEW)
-        self.main_frame.rowconfigure(0, weight=1)
-        self.main_frame.columnconfigure(0, weight=1)
+        # Set dialog properties: title, fixed size, close button disabled:
+        self.title(self._TITLE) # Dialog title for JDK installation
+        self.resizable(height=tk.FALSE, width=tk.FALSE) # Prevent its resizing
+        self.protocol('WM_DELETE_WINDOW', '{#}') # Disable its close button
 
-        self.title(self._TITLE)
-        self.resizable(height=tk.FALSE, width=tk.FALSE)
-        self.protocol('WM_DELETE_WINDOW', '{#}') # Block window close button
+        # Window/Frame:
+        main_frame = self.main_frame = ttk.Frame(self)
+        main_frame.grid(ipadx=15, ipady=15, sticky=tk.NSEW)
+        main_frame.rowconfigure(0, weight=1)
+        main_frame.columnconfigure(0, weight=1)
 
         # Display install message:
-        message_label = ttk.Label(self.main_frame, text=self._INSTALL_JDK)
+        message_label = ttk.Label(main_frame, text=self._INSTALL_JDK)
         message_label.grid(pady=0, columnspan=2)
 
-        # OK button:
-        self.ok_button = ttk.Button(
-            self.main_frame,
+        # OK proceed button:
+        ok_button = self.ok_button = ttk.Button(
+            main_frame,
             text=self._OK,
             command=self._proceed,
             default=tk.ACTIVE)
 
-        self.ok_button.grid(
-            row=2, column=0,
-            padx=15, pady=15,
-            sticky=tk.W)
-
-        self.ok_button.focus_set()
+        ok_button.grid(row=2, column=0, padx=15, pady=15, sticky=tk.W)
+        ok_button.focus_set()
 
         # Cancel button:
-        self.cancel_button = ttk.Button(
-            self.main_frame,
+        cancel_button = self.cancel_button = ttk.Button(
+            main_frame,
             text=self._CANCEL,
             command=self._close)
 
-        self.cancel_button.grid(
-            row=2, column=1,
-            padx=15, pady=15,
-            sticky=tk.E)
+        cancel_button.grid(row=2, column=1, padx=15, pady=15, sticky=tk.E)
 
 
     def _proceed(self) -> None:
         '''Starts JDK downloader thread.'''
+
         # Get rid of both OK & Cancel buttons:
         if self.ok_button: self.ok_button.destroy()
         if self.cancel_button: self.cancel_button.destroy()
 
         # Progress bar label:
         dl_label = ttk.Label(self.main_frame, text=self._PROGRESS)
-        dl_label.grid(row=1, columnspan=2, pady=self._PAD)
+        dl_label.grid(row=1, columnspan=2, pady=self._PROGRESS_BAR_Y_PADDING)
 
         # Progress bar:
         progress_bar = ttk.Progressbar(self.main_frame, mode='indeterminate')
 
         progress_bar.grid(
             row=2, column=0, columnspan=2,
-            padx=15, pady=self._PAD,
+            padx=15, pady=self._PROGRESS_BAR_Y_PADDING,
             sticky=tk.EW)
 
-        # Start progress bar animation and download thread:
+        # Start progress bar animation + download thread:
         if self.main_frame: self.main_frame.tkraise()
 
         download_thread = DownloadJDK()
@@ -241,10 +251,12 @@ class JdkDialog(ui_utils.CommonDialog):
 
     def _monitor(self, download: Thread, progress: ttk.Progressbar) -> None:
         '''Animate progress bar while JDK installs and extracts.'''
+
         if download.is_alive():
             self.after(100, lambda: self._monitor(download, progress))
             return
 
+        # Destroy this JDK dialog instance once download has finished:
         progress.stop()
         self._close()
 
@@ -260,14 +272,14 @@ class JdkDialog(ui_utils.CommonDialog):
 
 class DownloadJDK(Thread):
     '''Background thread for downloading & installing JDK into Thonny's folder.
-
     - Removes any preexisting JDK folders matching the expected version.
     - Downloads and extracts the required JDK version.
     - Renames the downloaded folder to the expected format.
-    - Sets JAVA_HOME both in system environment and Thonny configuration.
-    '''
+    - Sets JAVA_HOME on Thonny configuration.'''
+
     def run(self) -> None:
-        '''Download and setup JDK (installs to Thonny's config directory)'''
+        '''Download and setup JDK (installs to Thonny's user directory)'''
+
         # Delete existing Thonny's JDK subfolders matching jdk-<version##>:
         self.process_matchjdk_dirs(shutil.rmtree)
 
@@ -283,6 +295,7 @@ class DownloadJDK(Thread):
     @staticmethod
     def process_matchjdk_dirs(action: PathAction, only_1st=False) -> None:
         '''Apply an action to JDK-matching subfolders in Thonny's folder.'''
+
         for path in DownloadJDK.get_all_thonny_folder_paths():
             if path.name.startswith(JDK_DIR): # Folder name matches <jdk-##> 
                 action(path) # Callback to run on each matching folder path
