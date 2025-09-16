e590b3ea36db0779f6562502fefb5fa367e498c8
GoToLoop <EMLF@HotMail.com>
2025-09-16 04:11:02 -0300

    Mostly done

--- thonnycontrib/backend/py5_imported_mode_backend.py
+++ thonnycontrib/backend/py5_imported_mode_backend.py
@@ -4,9 +4,9 @@
 from os import environ as env
 from sys import path
 
-from typing import Optional, TypedDict, TypeAlias
+from typing import TypeAlias
 
-from thonny.common import InlineCommand, InlineResponse, CompletionInfo
+from thonny.common import CompletionInfo
 from thonny.plugins.cpython_backend import MainCPythonBackend
 from thonny import jedi_utils, get_sys_path_directory_containg_plugins
 
@@ -14,35 +14,30 @@ path.append( get_sys_path_directory_containg_plugins() )
 
 AutoComplete: TypeAlias = dict[str, list[CompletionInfo] | str | None]
 
-class Command(TypedDict):
-    source: str
-    row: int
-    column: int
-    filename: str
-    sys_path: Optional[list[str]]
-
-def patched_editor_autocomplete(self: MainCPythonBackend, cmd: Command) -> AutoComplete:
+def patched_editor_autocomplete(self: MainCPythonBackend, cmd) -> AutoComplete:
     '''Add py5 to autocompletion'''
 
     prefix = 'from py5 import *\n'
 
-    cmd['source'] = prefix + cmd['source']
-    cmd['row'] += 1
+    cmd.source = prefix + cmd.source
+    cmd.row += 1
 
-    result: Command = dict(
+    completions = jedi_utils.get_script_completions(
+        cmd.source,
+        cmd.row,
+        cmd.column,
+        cmd.filename,
+        sys_path=[get_sys_path_directory_containg_plugins()])
+
+    cmd.row -= 1
+    cmd.source = cmd.source[len(prefix):]
+
+    return dict(
         source=cmd.source,
         row=cmd.row,
         column=cmd.column,
-        filename=cmd.filename
-    )
-
-    result['completions'] = jedi_utils.get_script_completions(
-        **result, sys_path=[get_sys_path_directory_containg_plugins()])
-
-    result['row'] -= 1
-    result['source'] = result['source'][len(prefix):]
-
-    return result
+        filename=cmd.filename,
+        completions=completions)
 
 
 def load_plugin() -> None:
@@ -54,5 +49,4 @@ def load_plugin() -> None:
     # https://groups.Google.com/g/thonny/c/wWCeXWpKy8c
     c_e_a = MainCPythonBackend._cmd_editor_autocomplete
     setattr(MainCPythonBackend, '_original_editor_autocomplete', c_e_a)
-    # MainCPythonBackend._original_editor_autocomplete = c_e_a
     MainCPythonBackend._cmd_editor_autocomplete = patched_editor_autocomplete
